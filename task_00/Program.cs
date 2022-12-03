dfe959b231325c043e3d706f887c2d854a5bee2c
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-03 11:44:11 +0300

    Add Task_00 task_001

--- task_00/Program.cs
+++ task_00/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine("ДА");
+}
+else Console.WriteLine("Нет");
