22dcea8b2acdaee8227aee78b5a5dcc44e171c4e
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-04 13:40:02 +0300

    Quarte

--- Seminar_02/Program.cs
+++ Seminar_02/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите координату x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y");
+int y = Convert.ToInt32(Console.ReadLine());
+string FindQuarte(int xc, int yc)
+{
+
+    if (xc > 0 && yc > 0) return "1 Четверть";
+    if (xc < 0 && yc < 0) return "3 четверть";
+    if (xc < 0 && yc > 0) return "2 четверть";
+    if (xc > 0 && yc < 0) return "4 четверть";
+    return "Введеы некор корд";
+}
+string result = FindQuarte(x, y);
+Console.WriteLine(result);
