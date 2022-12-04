d015e3028a7a154393584298d1b02e52870de97c
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-04 12:49:54 +0300

    New project

---
+++ Seminar_01/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите координату x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x != 0 && y !=0)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("1 Четверть");
+    }
+    if (x < 0 && y < 0)
+    {
+        Console.WriteLine("3 четверть");       
+    }
+    if (x < 0 && y > 0)
+    {
+        Console.WriteLine("2 четверть");
+    }
+    if (x > 0 && y < 0)
+    {
+        Console.WriteLine("4 четверть");
+    }
+}
+
