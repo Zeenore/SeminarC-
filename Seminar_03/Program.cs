106fa027ac742794114dac30be932f6cbe2625a2
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-04 13:58:37 +0300

    Сделал задание

--- Seminar_03/Program.cs
+++ Seminar_03/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер четверти");
+int x = Convert.ToInt32(Console.ReadLine());
+string Quarty(int q)
+{
+    if (q == 1) return ("x больше нуля, y больше нуля");
+    if (q == 2) return ("х меньше нуля и у больше нуля");
+    if (q == 3) return ("х меньше нуля и у меньше нуля");
+    if (q == 4) return ("х больше нуля и у меньше нуля");
+    return "Введена не коректная четверть";
+}
+string result = Quarty(x);
+Console.WriteLine(result);
