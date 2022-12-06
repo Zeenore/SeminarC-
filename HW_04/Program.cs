8f3a0211137f6646f153ed5e62f40893f16511d7
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-06 15:00:36 +0300

    Семинар новый

---
+++ HW_04/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7 };
+void Print(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+Print(arr);
+void selectionsort(int[] arra)
+{
+    for (int i = 0; i < arra.Length; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if(arr[j] < arr[minPosition]) minPosition = j;
+        }
+        int temp = arra[i];
+        arra[i] = arra[minPosition];
+        arra[minPosition] = temp;
+    }
+}
+Print(arr);
+selectionsort(arr);
+Print(arr);
---
+++ seminar_05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+
+int result = 0;
+
+for (int i = 1; i <= a; i++)
+{
+    result += i;
+    
+}
+
+Console.WriteLine(result);
