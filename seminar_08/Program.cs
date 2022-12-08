ebe9e5085aacce397e98466e8743ab326e21da13
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-08 13:05:14 +0300

    321

--- seminar_08/Program.cs
+++ seminar_08/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreatArrayRandomIntFill(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+        
+    }
+    return arr;
+}
+
+
+int[] SummaOtricatAndPolojt(int[] arr)
+{
+    int summaPolojit = 0;
+    int summaOtricat = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0)
+        {
+            summaOtricat = summaOtricat + arr[i];
+        }
+        else
+        {
+            summaPolojit += arr[i];
+        }
+    }
+    return new int[]{summaPolojit, summaOtricat};
+}
+void PrintArray(int[] arraya)
+{
+    for (int i = 0; i < arraya.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arraya.Length - 1) Console.Write(arraya[i] + ",");
+        else Console.Write(arraya[i] + "]");
+    }
+}
+
+int[] array = CreatArrayRandomIntFill(24, -50, 50);
+PrintArray(array);
+int[] sumPolojitOtricat = SummaOtricatAndPolojt(array);
+Console.WriteLine();
+PrintArray(sumPolojitOtricat);
+
