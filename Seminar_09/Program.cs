afd3effc6af9ebb0425220b44b27e983eebeecd4
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-08 13:35:55 +0300

    123213

---
+++ Seminar_09/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу замены элементов массива:
+// положительные элементы заменить на соответствующие отрицательные
+// и наоборот
+// [-4, -8, 8, 2] [4, 8, -8, 2]
+
+
+
+int[] array = {-4, -8, 8, 2};
+
+
+int[] ElementZamena(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+    return arr;
+}
+
+void Print(int[] arra)
+{
+    for (int i = 0; i < arra.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arra.Length - 1) Console.Write(arra[i] + ",");
+        else Console.Write(arra[i] + "]");
+    }
+}
+int[] array1 = ElementZamena(array);
+Console.WriteLine(array);
+Print(array1);
