0417c9714aa0f238009b21c7ccf50a229649fa47
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-13 18:40:56 +0300

    quest

---
+++ HW_05/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+
+int NumberUpZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count++;
+    }
+    return count;
+}
+NumberUpZero(arr);
+ int result = NumberUpZero(arr);
+ Console.WriteLine($"Кол-во введённых числе больше 0 -> {result}");
+
+
+
+
+
+
