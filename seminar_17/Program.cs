0746207b2242033639143229a88503998490ade0
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-17 01:29:12 +0300

    general menegment

--- seminar_17/Program.cs
+++ seminar_17/Program.cs
@@ -1,45 +1,30 @@
-﻿int[,] CreatMatrixRandomInt(int m, int n, int min, int max)
+﻿double[,] NewMatrixRandomDoubels(int m, int n, int min, int max)
 {
-    int[,] arr = new int[n, m];
+    double[,] array = new double[m,n];
     Random rnd = new Random();
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int k = 0; k < arr.GetLength(1); k++)
+        for (int k = 0; k < array.GetLength(1); k++)
         {
-            arr[i, k] = rnd.Next(min, max + 1);
+            array[i,k] = rnd.Next(min, max + 1);
         }
     }
-    return arr;
+    return array;
 }
 
-int[,] MatrixChetnostb(int[,] arra)
+
+void PrintMatrix(double[,] arra)
 {
     for (int i = 0; i < arra.GetLength(0); i++)
     {
-        for (int k = 0; k < arra.GetLength(1); k++)
-        {
-            if (i % 2 == 0 && k % 2 == 0); arra[i,k] = arra[i,k] * arra[i,k];
-        }
-    }
-    return arra;
-}
-
-
-void PrintMatrix(int[,] arry)
-{
-    for (int i = 0; i < arry.GetLength(0); i++)
-    {
-        for (int j = 0; j < arry.GetLength(1); j++)
+        for (int j = 0; j < arra.GetLength(1); j++)
         {
             if (j == 0) Console.Write("[");
-            if (j < arry.GetLength(1) - 1) Console.Write(arry[i,j] + ",");
-            else Console.Write(arry[i,j] + "]");
+            if (j < arra.GetLength(1) - 1) Console.Write(arra[i,j] + "  ");
+            else Console.Write(arra[i,j] + "]");
         }
         Console.WriteLine();
     }
 }
-int[,] result = CreatMatrixRandomInt(3,4,1,23);
-PrintMatrix(result);
-Console.WriteLine();
-result = MatrixChetnostb(result);
-PrintMatrix(result);
+double[,] ar = NewMatrixRandomDoubels(4, 5, 1, 12);
+PrintMatrix(ar);
