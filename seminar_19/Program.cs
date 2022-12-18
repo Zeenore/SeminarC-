394bd571757fccbca2566a851a670b9923595518
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-18 14:15:45 +0300

    213

---
+++ seminar_19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Задайте число от:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число до:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Lol(int mo, int no)
+{
+    if (no == mo - 1) return;
+    Lol(mo, no - 1);
+    Console.Write($"{no} ");
+}
+Lol(m, n);
